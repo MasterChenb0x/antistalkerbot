412053dbf7359ee8d591a78dc2624bbc75d1880f
chenb0x <artofmorellc@gmail.com>
2018-06-29 01:25:10 -0700

    Working with menu items.

--- README.md
+++ README.md
@@ -7,8 +7,13 @@ Antidote to my 2015 DC Skytalk "Automate Your Stalking".
 ###### 06/25/2018 
 Currently, all testing is done in a separate working directory. Code will be merged when testing complete.
 
-_TO-DO_
-1. Build menu into program.
-2. Report default settings of suspect accounts.
-3. Resolve Twitter IDs to usernames for human readability
+###### 06/28/2018
+Moved a lot of snippets around this session. Worked on a menu system so input is taken in as an argument, either by username or user ID. I can make the menu more functional, but for now it works with minimal iptions.
 
+Taking in username argument seems to work, but there is no output with user ID as an argument. Tried str and int formatting for ID, but no dice. Will troubleshoot this soon. The rest of my sandbox is in the giant multiline comment towards the end of the script. That will disappear as I move snippets around.
+
+_TO-DO_
+1. ~~Build menu into program.~~
+2. Report default settings of suspect accounts.
+3. Resolve Twitter IDs to usernames for human readability.
+4. Fix User ID query.
--- antistalker.py
+++ antistalker.py
@@ -3,17 +3,17 @@ import sys
 import os
 import re
 import random
-import  time
+import time
 import getopt
 from datetime import datetime
 from twython import Twython
 
 
 #-- Twitter instance setup
-apiKey = 'YOUR_TWITTER_API_KEY'
-apiSecret = 'YOUR_TWITTER_API_SECRET'
-accessToken = 'YOUR_TWITTER_ACCESS_TOKEN'
-accessTokenSecret = 'YOUR_TWITTER_ACCESS_TOKEN_SECRET'
+apiKey = 'YOUR_API_KEY'
+apiSecret = 'YOUR_API_SECRET'
+accessToken = 'YOUR_ACCESS_TOKEN'
+accessTokenSecret = 'YOUR_ACCESS_TOKEN_SECRET'
 
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
 #--
@@ -23,8 +23,10 @@ def usage():
 	"""
 	Displays options and examples upon running with bad or missing params
 	"""
-	print("Usage: antiStalker.py options \n")
-	print("	-u/--username,-i/--userid : Username of person potentially being stalked \n")
+	print("Usage: ./antiStalker.py options \n")
+	print("		-u : Twitter handle of Stalker's Target...probably your own handle \n")
+	print("		-i : Same as -u but with ID instead of handle \n")
+	sys.exit()
 
 def getUserInfobyID(userid):
 	""" 
@@ -64,51 +66,78 @@ def getFriendsbyID(userid):
         """
         return api.get_friends_ids(user_id=userid)
 
+def start(argv):
+	if len(sys.argv) < 1:
+		usage()
+		sys.exit()
+	try:
+		opts, args = getopt.getopt(argv, "u:i")
+	except getopt.GetoptError:
+		usage()
+		sys.exit()
+	except Exception, e:
+		pass
+	twitName = ""
+	twitID = ""
+	flist = open("followerlist.txt","r+")
+	slist = open("suspects.txt","r+")
+	followerArray = []
+	suspectsArray = []
+	for opt, arg in opts:
+		if opt == '-u':
+			twitName = str(arg)
+		elif opt == '-i':
+			twitID = int(arg)			
+	if twitName != "":
+		result = getUserInfobyName(twitName)
+		print(result)
+	elif twitID != "":
+		result = getUserInfobyID(twitID)
+		print(result)
+	flist.close()
+	slist.close()
+	sys.exit()
 
 #--
 
-# api.update_status(status="Test") 
-
-flist = open("followerlist.txt","r+")
-slist = open("suspects.txt","r+")
-
-followerArray = []
-suspectsArray = []
-
+'''
 #-- Still using the below area for testing until I can put it all together.
 result = getUserInfobyID(00000000)
 sName = result['screen_name']
-print result
+print sName
 
 
-followers = getFollowersbyName('target_name')
+followers = getFollowersbyName('target')
 followerArray = followers['ids']
 print followerArray
 
-
+# Write followers to file for later analysis.
 for fa in followerArray:
 	flist.write(str(fa) + "\n")
 
+# Grab followers of followers to build a list of potential suspects.
 for i in followerArray:
-	print(i)
+	print("Retrieving " + str(i) + "\'s followers")
 	try:
 		followerDictObj = getFollowersbyID(i)
 		suspectsArray = followerDictObj['ids']
 		for sa in suspectsArray:
 			slist.write(str(sa) + "\n")
-	except:
+	except: # private accounts. This may still have potential
 		slist.write(str(i) + " may be a protected account. \n")
 		pass
-	time.sleep(60)
+	time.sleep(30) # should avoid rate limit
 
-
-#for i in followerArray:
-#	r = getUserInfobyID(i)
-#	nameres = r['screen_name']
-#	print "Writing " + nameres + " to file..." "\r\n"
-#	flist.write(str(r['id']) + "\n")
-#	time.sleep(15)
 
 flist.close()
 slist.close()
-	
+
+'''
+if __name__ == "__main__":
+	try:
+		start(sys.argv[1:])
+	except KeyboardInterrupt:
+		print("Program stopped by user.")
+	except:
+		sys.exit()	
+
